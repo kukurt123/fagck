2d1f9eac0af977783be6784270283838924ffd44
kukurt123 <kurtestacion@yahoo.com>
2020-02-06 22:22:46 +0800

    '11th'

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.static(distDir));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use("backend/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
